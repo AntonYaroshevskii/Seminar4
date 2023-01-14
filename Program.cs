c0d5ddb24e9e37390aed62fbec48b875a195398d
AntonYaroshevskii <120041153+AntonYaroshevskii@users.noreply.github.com>
2023-01-14 23:10:47 +0300

    homework

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+  /*Console.Write("Введите число A: ");
+  int numA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numB = Convert.ToInt32(Console.ReadLine());
+  int sum = GetNum(numA, numB);
+  Console.WriteLine("Ответ: " + sum);*/
+
+
+
+
+//-------------метод-------------
+
+/*int GetNum (int numA, int numB){
+    int result = 1;
+    for(int i = 1; i <= numB; i++){
+        result = result * numA; 
+    }
+    return result;  
+}*/
+  
+
+ 
+  //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+/*Console.Write("Введите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+int sumNumber = GetNum(numA);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);*/
+
+
+
+//----------метод----------
+
+/*int GetNum(int numA){
+    
+    int counter = Convert.ToString(numA).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numA - numA % 10;
+      result = result + (numA - advance);
+      numA = numA / 10;
+    }
+   return result;
+  }*/
+
+
+
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (числа берете любые)
+
+
+
+
+
+
+
+//---------метод------------
